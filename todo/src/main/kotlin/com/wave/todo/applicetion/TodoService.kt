8d250bbd0164d30c4a9852d65538bcec5cdafa8a
waveofmymind <sjun0913@gmail.com>
2023-04-30 03:40:24 +0900

    [FEAT] 서비스 리팩토링

---
+++ todo/src/main/kotlin/com/wave/todo/applicetion/TodoService.kt
@@ -0,0 +1,54 @@
+package com.wave.todo.applicetion
+
+import com.wave.todo.domain.Todo
+import com.wave.todo.domain.TodoRepository
+import com.wave.todo.dto.TodoRequest
+import org.springframework.data.domain.Sort
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDateTime
+
+@Service
+class TodoService(
+    val todoRepository: TodoRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun findAll(): List<Todo> =
+        todoRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): Todo =
+        todoRepository.findByIdOrNull(id)
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
+    @Transactional
+    fun create(request: TodoRequest?): Todo {
+        checkNotNull(request) { "TodoRequest is null" }
+        val Todo = Todo(
+            title = request.title,
+            description = request.description,
+            done = request.done,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+        )
+
+        return todoRepository.save(Todo)
+    }
+
+    @Transactional
+    fun update(id: Long, request: TodoRequest?): Todo {
+        checkNotNull(request) { "TodoRequest is null" }
+
+        return findById(id).let {
+            it.update(request.title, request.description, request.done)
+            todoRepository.save(it)
+        }
+    }
+
+    fun delete(id: Long) = todoRepository.deleteById(id)
+
+}
