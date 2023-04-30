70c0d3a1fa2f7de6bb3c77b08a47fe090661df63
waveofmymind <sjun0913@gmail.com>
2023-04-30 18:42:36 +0900

    웹플럭스 공부 시작

---
+++ webflux-example/src/main/kotlin/com/wave/webfluxexample/WebfluxExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.wave.webfluxexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxExampleApplication>(*args)
+}
