3d30dd1515a3ca70e8b22a262c9506d2b233ac97
waveofmymind <sjun0913@gmail.com>
2023-05-03 23:36:02 +0900

    [FEAT] r2dbc 관련 학습

--- webflux-example/src/main/kotlin/com/wave/webfluxexample/WebfluxExampleApplication.kt
+++ webflux-example/src/main/kotlin/com/wave/webfluxexample/WebfluxExampleApplication.kt
@@ -1,10 +1,28 @@
 package com.wave.webfluxexample
 
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+
 
 @SpringBootApplication
-class WebfluxExampleApplication
+class WebfluxExampleApplication{
+    @Bean
+    fun init(connectionFactory: ConnectionFactory) {
+        var connectionFactory =
+            ConnectionFactories.get("r2dbc:h2:mem:///test?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+        ConnectionFactoryInitializer().apply {
+            setConnectionFactory(connectionFactory)
+            setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+        }
+    }
+    
+}
 
 fun main(args: Array<String>) {
     runApplication<WebfluxExampleApplication>(*args)
---
+++ webflux-example/src/main/resources/scripts/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS book;
+CREATE TABLE book (
+    id bigint NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    price int,
+    primary key (id)
+);
