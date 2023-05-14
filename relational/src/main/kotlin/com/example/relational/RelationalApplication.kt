d81dbd2bfcf0e0319082df1b72b5ba71da11186d
waveofmymind <sjun0913@gmail.com>
2023-05-14 18:12:36 +0900

    [FEAT] 일대일 학습 예제 시작

---
+++ relational/src/main/kotlin/com/example/relational/RelationalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.relational
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RelationalApplication
+
+fun main(args: Array<String>) {
+    runApplication<RelationalApplication>(*args)
+}
