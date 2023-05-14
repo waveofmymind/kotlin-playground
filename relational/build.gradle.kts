cf60671f6f381c29de65a5db58c41f5ebaaa9437
waveofmymind <sjun0913@gmail.com>
2023-05-15 00:46:51 +0900

    [FEAT] mysql 의존성 추가 및 yaml 파일 작성

--- relational/build.gradle.kts
+++ relational/build.gradle.kts
@@ -21,7 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
